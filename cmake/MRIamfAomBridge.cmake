5928deb25d21587c88feb3bf8634a78e705af8c8
Sakuzy _Peng <86222913+SakuzyPeng@users.noreply.github.com>
2026-05-29 20:48:15 +0800

    ci: add windows iamf bridge prebuild

--- cmake/MRIamfAomBridge.cmake
+++ cmake/MRIamfAomBridge.cmake
@@ -14,16 +14,36 @@ function(mr_adm_core_find_iamf_aom_bridge)
         PATH_SUFFIXES lib lib64
         NO_CACHE)
 
+    if(WIN32)
+        find_file(MR_ADM_IAMF_AOM_BRIDGE_RUNTIME
+            NAMES mr_iamf_aom_bridge.dll libmr_iamf_aom_bridge.dll
+            HINTS ${_mr_iamf_hints}
+            PATH_SUFFIXES bin lib lib64
+            NO_CACHE)
+    endif()
+
     if(NOT MR_ADM_IAMF_AOM_BRIDGE_LIBRARY)
         message(FATAL_ERROR
             "MR_ADM_ENABLE_IAMF=ON requires the official AOM iamf-tools prebuilt bridge. "
-            "Set MR_ADM_IAMF_AOM_ROOT to a directory containing lib/libmr_iamf_aom_bridge.*")
+            "Set MR_ADM_IAMF_AOM_ROOT to an SDK directory containing lib/[lib]mr_iamf_aom_bridge.*")
+    endif()
+    if(WIN32 AND NOT MR_ADM_IAMF_AOM_BRIDGE_RUNTIME)
+        message(FATAL_ERROR
+            "MR_ADM_ENABLE_IAMF=ON requires the official AOM iamf-tools bridge runtime DLL. "
+            "Set MR_ADM_IAMF_AOM_ROOT to an SDK directory containing bin/mr_iamf_aom_bridge.dll")
     endif()
 
     add_library(MacinRender::IamfAomBridge UNKNOWN IMPORTED GLOBAL)
-    set_target_properties(MacinRender::IamfAomBridge PROPERTIES
-        IMPORTED_LOCATION "${MR_ADM_IAMF_AOM_BRIDGE_LIBRARY}"
-    )
+    if(WIN32)
+        set_target_properties(MacinRender::IamfAomBridge PROPERTIES
+            IMPORTED_IMPLIB "${MR_ADM_IAMF_AOM_BRIDGE_LIBRARY}"
+            IMPORTED_LOCATION "${MR_ADM_IAMF_AOM_BRIDGE_RUNTIME}"
+        )
+    else()
+        set_target_properties(MacinRender::IamfAomBridge PROPERTIES
+            IMPORTED_LOCATION "${MR_ADM_IAMF_AOM_BRIDGE_LIBRARY}"
+        )
+    endif()
 
     message(STATUS "Using AOM iamf-tools bridge: ${MR_ADM_IAMF_AOM_BRIDGE_LIBRARY}")
 endfunction()
